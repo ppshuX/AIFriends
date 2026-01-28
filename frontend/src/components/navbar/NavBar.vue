5d4306077a94e7ac7d481cf181e05786c3650cf8
wenxiao Lv <2064747320@qq.com>
2026-01-28 15:07:28 +0800

    feat: complete user menu (space, edit profile, logout) and add icons

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -21,8 +21,15 @@
         </div>
 
         <div class="navbar-end">
-          <RouterLink :to="{ name: 'register-index' }" active-class="Button-active" class="btn btn-ghost text-lg">注册</RouterLink>
-          <RouterLink :to="{ name: 'login-index' }" active-class="Button-active" class="btn btn-ghost text-lg">登录</RouterLink>
+          <RouterLink v-if="user.isLogin()" :to="{ name: 'create-index' }" active-class="Button-active" class="btn btn-ghost text-lg">
+            <CreateIcon />
+            创作
+          </RouterLink>
+
+          <RouterLink v-if="!user.isLogin()" :to="{ name: 'login-index' }" active-class="Button-active" class="btn btn-ghost text-lg">
+            登录
+          </RouterLink>
+          <UserMenu v-if="user.isLogin()" />
         </div>
       </nav>
       <slot></slot>
@@ -64,6 +71,10 @@ import HomepageIcon from "@/components/navbar/icons/HomepageIcon.vue";
 import FriendIcon from "@/components/navbar/icons/FriendIcon.vue";
 import CreateIcon from "@/components/navbar/icons/CreateIcon.vue";
 import SearchIcon from "@/components/navbar/icons/SearchIcon.vue";
+import {useUserStore} from "@/stores/user.js";
+import UserMenu from "@/components/navbar/UserMenu.vue";
+
+const user = useUserStore();
 </script>
 
 <style scoped>
---
+++ frontend/src/components/navbar/UserMenu.vue
@@ -0,0 +1,57 @@
+<script setup>
+import useUserMenu from "./useUserMenu";
+import UserSpaceIcon from "@/components/navbar/icons/UserSpaceIcon.vue";
+import UserProfileIcon from "@/components/navbar/icons/UserProfileIcon.vue";
+import UserLogoutIcon from "@/components/navbar/icons/UserLogoutIcon.vue";
+
+const user = useUserMenu();
+
+function closeMenu() {
+  const element = document.activeElement
+  if (element && element instanceof HTMLElement) element.blur()
+}
+</script>
+
+<template>
+<div class="dropdown dropdown-end">
+  <div tabindex="0" role="button" class="avatar btn btn-circle w-8 h-8 mr-6">
+    <div class="w-8 rounded-full">
+      <img :src="user.photo" alt="">
+    </div>
+  </div>
+  <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li>
+      <RouterLink @click="closeMenu" :to="{name: 'user-space-index', params: {user_id: user.id}}">
+        <div class="avater">
+          <div class="w-10 rounded-full">
+            <img src="use.photo" alt="">
+          </div>
+        </div>
+        <span class="text-base font-bold line-clamp-1">{{ user.username }}</span>
+      </RouterLink>
+    </li>
+    <li>
+      <RouterLink @click="closeMenu" :to="{name: 'user-space-index', params: {user_id: user.id}}" class="text-sm font-bold py-3">
+        <UserSpaceIcon />
+        个人空间
+      </RouterLink>
+    </li>
+    <li>
+      <RouterLink @click="closeMenu" :to="{ name: 'profile-index' }" class="text-sm font-bold py-3">
+        <UserProfileIcon />
+        编辑资料
+      </RouterLink>
+    </li>
+    <li>
+      <button type="button" class="text-sm font-bold py-3 w-full text-left" @click="user.logout()">
+        <UserLogoutIcon />
+        退出
+      </button>
+    </li>
+  </ul>
+</div>
+</template>
+
+<style scoped>
+
+</style>
