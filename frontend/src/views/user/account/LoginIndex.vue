681ba75bb6b25bee96a0760b939ecea5dad148a0
wenxiao Lv <2064747320@qq.com>
2026-01-28 11:11:25 +0800

    login and register form

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -2,8 +2,23 @@
 </script>
 
 <template>
-  <div class="login-page">
-    登录
+  <div class="login-page flex justify-center py-8">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend">登录</legend>
+
+      <label class="label">用户名</label>
+      <input type="text" class="input input-bordered w-full" placeholder="输入用户名" />
+
+      <label class="label">密码</label>
+      <input type="password" class="input input-bordered w-full" placeholder="输入密码" />
+
+      <button class="btn btn-neutral mt-4 w-full">登录</button>
+      <div class="flex justify-end mt-2">
+        <RouterLink :to="{ name: 'register-index' }" class="btn btn-sm btn-ghost text-base-content/70">
+          注册
+        </RouterLink>
+      </div>
+    </fieldset>
   </div>
 </template>
 
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -2,12 +2,30 @@
 </script>
 
 <template>
-  <div class="register-page">
-    注册
+  <div class="login-page flex justify-center py-8">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <legend class="fieldset-legend">注册</legend>
+
+      <label class="label">用户名</label>
+      <input type="text" class="input input-bordered w-full" placeholder="输入用户名" />
+
+      <label class="label">密码</label>
+      <input type="password" class="input input-bordered w-full" placeholder="输入密码" />
+
+      <label class="label">确认密码</label>
+      <input type="password" class="input input-bordered w-full" placeholder="输入确认密码" />
+
+      <button class="btn btn-neutral mt-4 w-full">注册</button>
+      <div class="flex justify-end mt-2">
+        <RouterLink :to="{ name: 'login-index' }" class="btn btn-sm btn-ghost text-base-content/70">
+          登录
+        </RouterLink>
+      </div>
+    </fieldset>
   </div>
 </template>
 
 <style scoped>
-.register-page {
+.login-page {
 }
 </style>
