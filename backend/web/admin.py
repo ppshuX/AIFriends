0d6c5da550bcf62f564c759c4f169451e760d0ce
wenxiao Lv <2064747320@qq.com>
2026-02-15 11:10:21 +0800

    CRUD api of character in the backend

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-
+from .models.character import Character
 from .models.user import UserProfile
 
 # Register your models here.
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', )
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', )
--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, re_path
+
+from .views.create.character.get_single import GetSingleCharacterView
+from .views.create.character.remove import RemoveCharacter
 from .views.index import index
 from .views.user.account.get_user_info import GetUserInfo
 from .views.user.account.logout import LogoutView
@@ -13,6 +16,11 @@ urlpatterns = [
     path("api/user/account/register/", Register.as_view()),
     path("api/user/account/refresh_token/", RefreshTokenView.as_view()),
     path("api/user/account/get_user_info/", GetUserInfo.as_view()),
+
+    path('api/create/character/create/', UpdateProfile.as_view()),
+    path('api/create/character/update/', UpdateProfile.as_view()),
+    path('api/create/character/remove/', RemoveCharacter.as_view() ),
+    path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
     path("", index, name="index"),
     path('api/user/profile/update/', UpdateProfile.as_view()),
     # 前端 history 模式：除 media/static/assets 外的任意路径都交给前端路由
